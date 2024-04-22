c4afc178020ce1ba99973585874a3864a733f73e
Burak <omerburakdogan16@gmail.com>
2024-04-22 11:49:06 +0300

    #24 - Made JWT token expiry date a very distant date (100 days) so they ≈never expire

--- src/main/java/com/yicem/backend/yicem/security/jwt/JwtUtils.java
+++ src/main/java/com/yicem/backend/yicem/security/jwt/JwtUtils.java
@@ -17,11 +17,11 @@ import java.util.Date;
 public class JwtUtils {
   private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-  @Value("${bezkoder.app.jwtSecret}")
+  @Value("${yicem.app.jwtSecret}")
   private String jwtSecret;
 
-  @Value("${bezkoder.app.jwtExpirationMs}")
-  private int jwtExpirationMs;
+  @Value("${yicem.app.jwtExpirationMs}")
+  private long jwtExpirationMs;
 
   public String generateJwtToken(Authentication authentication) {
 
