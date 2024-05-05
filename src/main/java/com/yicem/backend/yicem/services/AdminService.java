b88a7270852d8001b764a85337ac910fbfbd06a8
Ahmet Alperen Yılmazyıldız <46990789+AlperenYilmazyildiz@users.noreply.github.com>
2024-05-05 22:31:06 +0300

    Implemented delete endpoints

--- src/main/java/com/yicem/backend/yicem/services/AdminService.java
+++ src/main/java/com/yicem/backend/yicem/services/AdminService.java
@@ -9,8 +9,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
